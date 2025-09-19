d6460e542bfb69f0565ab2d7369c918f19bbceae
viktoriaruseva <ruseva130407@icloud.com>
2025-09-19 15:12:32 +0300

    1

---
+++ src/lab_02/arrays.py
@@ -0,0 +1,18 @@
+#1
+
+# s=list(map(int,input().split()))
+# if len(s)==0:
+#     print('ValueError')
+# else:
+#     print(min(s),max(s))
+# print(sorted(set(s)))
+
+#2
+# s=list(input().split())
+# print(sorted(set(s)))
+
+
+s=[1, 2]
+a=[2, 6]
+
+print(s+a)
