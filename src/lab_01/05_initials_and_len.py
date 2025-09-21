f0fdb86a97be6405df6ee299d130851e1651ee34
viktoriaruseva <ruseva130407@icloud.com>
2025-09-21 19:44:50 +0300

    .

--- src/lab_01/05_initials_and_len.py
+++ src/lab_01/05_initials_and_len.py
@@ -1,3 +1,4 @@
 name=str(input('ФИО: '))
 print(f'Инициалы: {(name.split()[0])[0]}{(name.split()[1])[0]}{(name.split()[2])[0]}.')
-print(f'Длина (символов):{len(name)}')
+name =name.replace(' ','')
+print(f'Длина (символов):{len(name)+2}')
