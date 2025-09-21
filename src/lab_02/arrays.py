5a394b318923ef792314c4a20764a6a3e5221629
viktoriaruseva <ruseva130407@icloud.com>
2025-09-21 20:53:59 +0300

    .

--- src/lab_02/arrays.py
+++ src/lab_02/arrays.py
@@ -11,8 +11,21 @@
 # s=list(input().split())
 # print(sorted(set(s)))
 
+#3
+a=[[1, 2], [1,0]]
+d=[]
+g=[]
+k=0
+for i in a:
+    for j in i:
+        d.append(j)
+        if type(j)==int:
+            k+=1
+        if k==len(d):
+            g.append(j)
+        else:
+            print('TypeError')
+            break
+if k==len(d):
+    print(g)
 
-s=[1, 2]
-a=[2, 6]
-
-print(s+a)
