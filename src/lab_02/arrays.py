32209ffcdd52f8cfb194444a561c4b5737d575cd
viktoriaruseva <ruseva130407@icloud.com>
2025-09-23 14:05:34 +0300

    .

--- src/lab_02/arrays.py
+++ src/lab_02/arrays.py
@@ -1,31 +1,46 @@
 #1
 
-# s=list(map(int,input().split()))
+# s=list(input().split())
+# d=[]
 # if len(s)==0:
 #     print('ValueError')
-# else:
-#     print(min(s),max(s))
-# print(sorted(set(s)))
+# for x in s:
+#     if '.' in x:
+#         d.append(float(x))
+#     else:
+#         d.append(int(x))
+# print(min(d), max(d))
 
 #2
-# s=list(input().split())
-# print(sorted(set(s)))
+
+a=[1.0, 1, 2.5, 2.5, 0]
+f=[]
+i=[]
+
+for p in a:
+    if type(p)==int:
+        i.append(p)
+    else:
+        f.append(p)
+res1=[x for x in f if int(x) in i]
+res2=[x for x in i if float(x)  not in f]
+print(set(sorted(res1+res2+f)))
 
 #3
-a=[[1, 2], [1,0]]
-d=[]
-g=[]
-k=0
-for i in a:
-    for j in i:
-        d.append(j)
-        if type(j)==int:
-            k+=1
-        if k==len(d):
-            g.append(j)
-        else:
-            print('TypeError')
-            break
-if k==len(d):
-    print(g)
+# a=[[1, 2], [1,0]]
+# d=[]
+# g=[]
+# k=0
+# for i in a:
+#     for j in i:
+#         d.append(j)
+#         if type(j)==int:
+#             k+=1
+#         if k==len(d):
+#             g.append(j)
+#         else:
+#             print('TypeError')
+#             break
+# if k==len(d):
+#     print(g)
 
--- src/lab_02/sd.py
+++
@@ -1,7 +0,0 @@
-if type(j) == int:
-    k += 1
-    if k == len(s):
-        g.append(j)
-    else:
-        print('TypeError')
-    break
