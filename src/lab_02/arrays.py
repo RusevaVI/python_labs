522bc8f681b18ee0ba5abf17e419e1d732a55a60
viktoriaruseva <ruseva130407@icloud.com>
2025-09-25 10:24:28 +0300

    .

--- src/lab_02/arrays.py
+++ src/lab_02/arrays.py
@@ -1,46 +1,80 @@
 #1
-
-# s=list(input().split())
+# a=[3,-1,5,5,0]
+# b=[42]
+# c=[-5, -2, -9]
 # d=[]
-# if len(s)==0:
-#     print('ValueError')
-# for x in s:
-#     if '.' in x:
-#         d.append(float(x))
+# e=[1.5, 2, 2.0, -3.1]
+# def f(b):
+#     t=tuple(b)
+#     s1=[]
+#     for i in t:
+#         s1.append(float(i))
+#     if len(s1)==0:
+#         print('ValueError')
 #     else:
-#         d.append(int(x))
-# print(min(d), max(d))
+#         s2=(min(s1),max(s1))
+#         print(s2)
+# print(f(a))
+# print(f(b))
+# print(f(c))
+# print(f(d))
+# print(f(e))
 
 #2
-
-a=[1.0, 1, 2.5, 2.5, 0]
-f=[]
-i=[]
-
-for p in a:
-    if type(p)==int:
-        i.append(p)
-    else:
-        f.append(p)
-res1=[x for x in f if int(x) in i]
-res2=[x for x in i if float(x)  not in f]
-print(set(sorted(res1+res2+f)))
+#
+# a=[3, 1, 2, 1, 3]
+# b=[]
+# c=[-1, -1, 0, 2, 2]
+# d=[1.0, 1, 2.5, 2.5, 0]
+# def f(l):
+#     i=[]
+#     f=[]
+#     for g in l:
+#         if type(g)==int:
+#             i.append(g)
+#         else:
+#             f.append(g)
+#     res1 = [x for x in f if int(x) in i]
+#     res2 = [x for x in i if float(x)  not in f]
+#     print(sorted(set(res1+res2+f)))
+# print(f(a))
+# print(f(b))
+# print(f(c))
+# print(f(d))
 
 #3
-# a=[[1, 2], [1,0]]
-# d=[]
-# g=[]
-# k=0
-# for i in a:
-#     for j in i:
-#         d.append(j)
-#         if type(j)==int:
-#             k+=1
-#         if k==len(d):
-#             g.append(j)
+# a = [[1, 2, 3]]
+# b = [[1], [2], [3]]
+# c = [[1, 2], [3, 4]]
+# d = []
+# e = [[1, 2], [3]]
+#
+#
+# def f1(x):
+#     h = []
+#     for i in x:
+#         if isinstance(i, list):
+#             for k in i:
+#                 h.append([k])
 #         else:
-#             print('TypeError')
-#             break
-# if k==len(d):
-#     print(g)
-
+#             h.append([i])
+#         return h
+#
+#
+# def f2(x):
+#     h = []
+#     for i in x:
+#         for j in i:
+#             h.append(j)
+#     return h
+#
+# def f3(x):
+#     for i in range(len(x) - 1):
+#         if len(x[i]) != len(x[i + 1]):
+#             print(ValueError)
+#
+#
+# print(f1(a))
+# print(f2(b))
+# print(f2(d))
+# print(f3(e))
