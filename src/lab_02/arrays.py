93354ba739b45860bee097fc1949bc7ac8759824
viktoriaruseva <ruseva130407@icloud.com>
2025-09-30 09:40:41 +0300

    .

--- src/lab_02/arrays.py
+++ src/lab_02/arrays.py
@@ -1,73 +1,75 @@
 #1
-a=[3,-1,5,5,0]
-b=[42]
-c=[-5, -2, -9]
-d=[]
-e=[1.5, 2, 2.0, -3.1]
-def f(b):
-    s1=[]
-    for i in b:
-        s1.append(i)
-    if len(s1)==0:
+test1=[3,-1,5,5,0]
+test2=[42]
+test3=[-5, -2, -9]
+test4=[]
+test5=[1.5, 2, 2.0, -3.1]
+def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
+    list1=[]
+    for i in nums:
+        list1.append(i)
+    if len(list1)==0:
         print('ValueError')
     else:
-        s2=(min(s1),max(s1))
-        print(s2)
+        res=(min(list1),max(list1))
+        print(res)
     return ''
-print(f(a))
-print(f(b))
-print(f(c))
-print(f(d))
-print(f(e))
+print(min_max(test1))
+print(min_max(test2))
+print(min_max(test3))
+print(min_max(test4))
+print(min_max(test5))
+
+
 
 #2
 
-a=[3, 1, 2, 1, 3]
-b=[]
-c=[-1, -1, 0, 2, 2]
-d=[1.0, 1, 2.5, 2.5, 0]
-def f(l):
-    i=[]
-    f=[]
-    for g in l:
-        if type(g)==int:
-            i.append(g)
+test1=[3, 1, 2, 1, 3]
+test2=[]
+test3=[-1, -1, 0, 2, 2]
+test4=[1.0, 1, 2.5, 2.5, 0]
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    int_list=[]
+    float_list=[]
+    for i in nums:
+        if type(i)==int:
+            int_list.append(i)
         else:
-            f.append(g)
-    res1 = [x for x in f if int(x) in i]
-    res2 = [x for x in i if float(x)  not in f]
-    print(sorted(set(res1+res2+f)))
+            float_list.append(i)
+    res1 = [x for x in float_list if int(x) in int_list]
+    res2 = [x for x in int_list if float(x)  not in float_list]
+    print(sorted(set(res1+res2+float_list)))
     return ''
-print(f(a))
-print(f(b))
-print(f(c))
-print(f(d))
+print(unique_sorted(test1))
+print(unique_sorted(test2))
+print(unique_sorted(test3))
+print(unique_sorted(test4))
 
-# #3
-a=[[1, 2], [3, 4]]
-b=([1, 2], (3, 4, 5))
-c=[[1], [], [2, 3]]
-d=[[1, 2], "ab"]
+#3
+test1=[[1, 2], [3, 4]]
+test2=([1, 2], (3, 4, 5))
+test3=[[1], [], [2, 3]]
+test4=[[1, 2], "ab"]
 
-def f(a):
-    x=[]
-    y=[]
+def flatten(mat: list[list | tuple]) -> list:
+    list1=[]
+    list2=[]
     k=0
-    for i in a:
-        for b in i:
-            x.append(b)
-            if type(b)==int:
+    for i in mat:
+        for j in i:
+            list1.append(j)
+            if type(j)==int:
                 k+=1
-            if k==len(x):
-                y.append(b)
+            if k==len(list1):
+                list2.append(j)
             else:
                 print('TypeError')
                 break
-    if k==len(x):
-        print(y)
+    if k==len(list1):
+        print(list2)
     return ''
 
-print(f(a))
-print(f(b))
-print(f(c))
-print(f(d))
+print(flatten(test1))
+print(flatten(test2))
+print(flatten(test3))
+print(flatten(test4))
