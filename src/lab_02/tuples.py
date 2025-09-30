cd86029a1d6f1596159854966366f3cbc596a11c
viktoriaruseva <ruseva130407@icloud.com>
2025-09-30 14:12:39 +0300

    .

--- src/lab_02/tuples.py
+++ src/lab_02/tuples.py
@@ -1,4 +1,5 @@
-def f(fio,group,gpa):
+def format_record(rec: tuple[str, str, float]) -> str:
+    fio,group,gpa=rec
     fio = fio.strip()
     name=[]
     if len(fio)==0 or len(group)==0 or type(gpa)!=float:
@@ -11,9 +12,9 @@ def f(fio,group,gpa):
             return (f'{(name[0])} {(name[1])[0]}.{(name[2])[0]}., гр. {group}, GPA {gpa:.2f}')
         else:
             return (f'{(name[0])} {(name[1])[0]}., гр. {group}, GPA {gpa:.2f}')
-print(f("Иванов Иван Иванович", "BIVT-25", 4.6))
-print(f("Петров Пётр", "IKBO-12", 5.0))
-print(f("Петров Пётр Петрович", "IKBO-12", 5.0))
-print(f("  сидорова  анна   сергеевна ", "ABB-01", 3.999))
-print(f("", "", 5))
+print(format_record(("Иванов Иван Иванович", "BIVT-25", 4.6)))
+print(format_record(("Петров Пётр", "IKBO-12", 5.0)))
+print(format_record(("Петров Пётр Петрович", "IKBO-12", 5.0)))
+print(format_record(("  сидорова  анна   сергеевна ", "ABB-01", 3.999)))
+print(format_record(("", "", 5)))
 
