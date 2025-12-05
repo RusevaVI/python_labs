0f13f6d3a82426087a419178f023e3811ffe883d
viktoriaruseva <ruseva130407@icloud.com>
2025-12-05 13:43:01 +0300

    .

--- test_lab_08.py
+++ test_lab_08.py
@@ -1,11 +1,8 @@
 from src.lab_08.serialize import students_to_json, students_from_json
 
-# 1. читаем студентов из входного JSON
-students = students_from_json("data1/lab08/students_input.json")
+students = students_from_json("data1/lab_08/students_input.json")
 
-# 2. пишем в выходной JSON
-students_to_json(students, "data1/lab08/students_output.json")
+students_to_json(students, "data1/lab_08/students_output.json")
 
-# 3. выводим на экран для проверки
 for s in students:
     print(s, "| age:", s.age())
